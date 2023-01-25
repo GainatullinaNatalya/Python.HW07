1c69cb9e1bf8946aed440afc52d6ac2fa78c27db
Nate <etlf74@yandex.ru>
2023-01-25 20:39:33 +0500

    Корректировка логирования

--- log.py
+++ log.py
@@ -1,20 +1,6 @@
-from datetime import datetime as dt
-from time import time
+
 import datetime
 
-def logger(mod):
+def logger(rezhim):
     with open('log.txt', 'a', encoding='utf-8') as file:
-        file.write(f'{mod} - {str(datetime.datetime.now())} \n')
-
-# def logger(rezhim):
-#     time = dt.now().strftime('%H:%M')
-#     with open('log.txt', 'w', encoding='utf-8') as file:
-#         file.write('{};pressure;{}\n'
-#                     .format(time, rezhim))
-#
-#
-# def wind_speed_logger(data):
-#     time = dt.now().strftime('%H:%M')
-#     with open('log.csv', 'a') as file:
-#         file.write('{};wind_speed;{}\n'
-#                     .format(time, data))
+        file.write(f'{rezhim} - {str(datetime.datetime.now())} \n')
